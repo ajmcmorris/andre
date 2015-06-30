2c6b88c36169acc6b595c7fa478bb07db537924c
jordan <ajmcmorris@gmail.com>
2015-06-30 11:08:58 -0400

    added new images and made site updates

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+	$('ul#filter a').click(function() {
+	  
+	  $('ul#filter .current').removeClass('current');
+	$(this).parent().addClass('current');
+	
+
+	var filterVal = $(this).text().toLowerCase().replace(' ','-');
+
+	if(filterVal == 'all') {
+	  $('section .item-list.hidden').fadeIn('slow').removeClass('hidden');
+	}else {
+	  $('section .item-list').each(function() {
+	    if(!$(this).hasClass(filterVal)) {
+	      $(this).fadeOut('slow').addClass('hidden');
+	    } else {
+	      $(this).fadeIn('slow').removeClass('hidden');
+	    }
+	  });
+	}
+		return false;
+	});
+});
